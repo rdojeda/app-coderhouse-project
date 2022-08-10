cc8489097161a6e207f30d533b3f3fb896dd979d
Roberto Ojeda <ojeda.robertodaniel@gmail.com>
2022-08-10 20:17:12 -0300

    Second First

--- src/config.js
+++ src/config.js
@@ -1,5 +1,19 @@
 import 'dotenv/config'
 
+
+export default {
+  mongoDB: {
+    URL: "mongodb+srv://Dbojedaweb:rdo0j3d6@cluster0.tsqrh.mongodb.net/ecommerce?retryWrites=true&w=majority",
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
+};
+
+
+
+/*
 export default {
   mongoDB: {
             URL: `mongodb+srv://${process.env.DB_USER_MONGO}:${
@@ -11,3 +25,4 @@ export default {
                     }
       }
 };
+*/
